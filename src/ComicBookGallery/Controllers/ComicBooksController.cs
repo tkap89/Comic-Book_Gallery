718829fdad5b8dc864516f4a50ad7eae494cea37
Tom kapon <tomkap89@gmail.com>
2017-07-07 16:00:18 +0200

    Added detail methode in the comic books controller

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,5 +8,9 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController:Controller
     {
+        public string Detail()
+        {
+            return "Hello from comic book controller";
+        }
     }
 }
