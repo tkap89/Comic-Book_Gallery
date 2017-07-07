f4cd9dd2b79bfc422ec4c99e680c79fa6e03ef60
Tom kapon <tomkap89@gmail.com>
2017-07-07 19:00:07 +0200

    updated layout

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,6 +10,16 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
+            ViewBag.Title = "The amazing spiderman";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "Spiderman movie";
+            ViewBag.Artits = new string[]
+                {
+                    "Artist 1 ",
+                    "Artist 2 ",
+                    "Artits 3 ",
+
+                };
 
             return View();
         }
