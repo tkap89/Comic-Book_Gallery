87765cc2ed9cae42c62f13836d1768750c43cc1c
Tom kapon <tomkap89@gmail.com>
2017-07-08 12:12:55 +0200

    updated comic book repository

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using ComicBookGallery.Models;
+using ComicBookGallery.Data;
 
 namespace ComicBookGallery.Controllers
 {
@@ -11,21 +12,9 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            var ComicBook = new ComicBook()
-            {
-                SeriesTitle = "The amazing spiderman",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives...<strong>will Peter Parker?</strong></p>",
-                Artists = new Artist[] 
-                { 
-                  new Artist() {Name = "Mike", role="Writer" },
-                  new Artist() {Name = "Luc", role= " publisher" },
-                  new Artist() {Name="marc",role="publisher" },
-                  new Artist() {Name="Jean", role="writer" }, 
-                  
-                }
-            };
-         
+
+            
+                     
 
             return View(ComicBook);
         }
